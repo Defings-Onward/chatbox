0eb540b24c83987d6c02180f028b985ded293d49
Ajayi Obanijesu <ajayiobanijesu05@gmail.com>
2025-06-02 22:22:32 +0100

    about to deploy

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from. import views
 urlpatterns = [
-    path("signup", views.signup, name="login"),
+    path("signup", views.signup, name="signup"),
     path("home", views.home, name='home'),
     path("room/<int:room_id>", views.room, name="room"),
     path("room", views.defs, name="def"),
+    path("", views.home, name="home")
 ]
