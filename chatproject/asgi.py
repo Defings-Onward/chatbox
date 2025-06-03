720985e2b2b7ea93eac9273cb614ca5d393105dd
Ajayi Obanijesu <ajayiobanijesu05@gmail.com>
2025-06-03 11:34:34 +0100

    Finished

--- chatproject/asgi.py
+++ chatproject/asgi.py
@@ -1,7 +1,8 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatproject.settings")
-
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chatapp.routing import wsPattern
 
